48f67421dd5f8afbf96db4f2075ebddf24068d11
VuTrungHieu2001 <88780876+VuTrungHieu2001@users.noreply.github.com>
2022-04-21 17:21:47 +0700

    update style ,icon

--- src/components/Heloo.jsx
+++ src/components/Heloo.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import {AiFillBug} from 'react-icons/ai'
 export default function Heloo() {
   return (
-    <div>heloo</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!  <AiFillBug/>
+    </h1>
   )
 }
